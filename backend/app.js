9c309661561de7a1987be2c8d56d85513704844a
Saikumar <109736498+saekumar@users.noreply.github.com>
2024-12-01 15:51:43 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const app = express()
 
 // Middleware
 const corsOptions = {
-  origin: ['http://localhost:5173', 'https://saenus.vercel.app/'],
+  origin: ['http://localhost:5173', 'https://saenus.vercel.app'],
   credentials: true,
 }
 
